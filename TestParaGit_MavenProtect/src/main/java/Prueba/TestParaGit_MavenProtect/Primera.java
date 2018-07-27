8a07dca2333a5455ff971875023b811f0e0ac573
ManuelMata <mmata@NE2675.cac.es>
2018-07-27 16:38:25 +0200

    Primera clase Java creada

---
+++ TestParaGit_MavenProtect/src/main/java/Prueba/TestParaGit_MavenProtect/Primera.java
@@ -0,0 +1,11 @@
+package Prueba.TestParaGit_MavenProtect;
+
+public class Primera {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Prueba");
+
+	}
+
+}
