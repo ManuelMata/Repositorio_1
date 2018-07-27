ee55a83ae5b16355f38435ff6037e33ce3ff4e0e
ManuelMata <mmata@NE2675.cac.es>
2018-07-27 16:28:42 +0200

    PRIMER COMMIT TRAS CREAR EL PROUECTO EN LOCAL

---
+++ TestParaGit_MavenProtect/src/main/java/Prueba/TestParaGit_MavenProtect/App.java
@@ -0,0 +1,13 @@
+package Prueba.TestParaGit_MavenProtect;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
