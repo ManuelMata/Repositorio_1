1d5ef15e737fc05ac305598822f3a1f34f9ac1ed
ManuelMata <mmata@NE2675.cac.es>
2018-07-27 16:41:51 +0200

    MODIFICACIÓN DE LA CLASE PRIMERA

--- TestParaGit_MavenProtect/src/main/java/Prueba/TestParaGit_MavenProtect/Primera.java
+++ TestParaGit_MavenProtect/src/main/java/Prueba/TestParaGit_MavenProtect/Primera.java
@@ -5,7 +5,7 @@ public class Primera {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Prueba");
-
+		System.out.println("Modificación de la clase Primera");
 	}
 
 }
